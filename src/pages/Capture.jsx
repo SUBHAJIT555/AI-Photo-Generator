d554f9c7205de169fbc15b88e57e84ca165eb657
Aditya <adityac5102@gmail.com>
2025-03-19 17:12:14 +0530

    update

--- src/pages/Capture.jsx
+++ src/pages/Capture.jsx
@@ -124,124 +124,71 @@ function Capture() {
   }, [columns, videoStream, startCamera]);
 
   return (
+    // <div
+    //   className="flex flex-col items-center w-full bg-cover bg-center h-screen min-h-screen text-white  justify-evenly"
+    //   style={{ backgroundImage: `url(${BGImage})` }}
+    // >
     <div
-      className="flex flex-col items-center w-full bg-cover bg-center h-screen min-h-screen text-white  justify-evenly"
+      className="flex flex-col items-center w-full h-screen justify-evenly"
       style={{ backgroundImage: `url(${BGImage})` }}
     >
-      <div className="flex flex-col items-center w-full h-screen justify-evenly">
-        <Logo />
+      <Logo />
 
-        {/* Hidden canvas for capturing the image */}
-        <canvas ref={canvasRef} style={{ display: "none" }} />
-        <div className="w-3/4" style={{}}>
-          {/* Video Stream Section */}
-          {!capturedImage ? (
-            <div className="flex flex-col items-center py-2">
-              <video
-                ref={videoRef}
-                className="bg-black shadow-lg rounded-2xl w-full"
-                autoPlay
-                muted
-              />
-              {countdown && (
-                <div className="absolute top-0 bottom-0 left-0 right-0 flex items-center justify-center bg-black bg-opacity-50 rounded-2xl">
-                  <p className="font-bold text-white animate-ping text-9xl">
-                    {countdown}
-                  </p>
-                </div>
-              )}
-            </div>
-          ) : (
-            // Captured Image Section
-            <div className="flex flex-col items-center py-2">
-              <img
-                src={capturedImage}
-                alt="Captured"
-                className="border-4 border-white shadow-lg rounded-2xl w-full"
-                style={{ width: "100%", height: "2025px" }}
-              />
-            </div>
-          )}
-        </div>
-
+      {/* Hidden canvas for capturing the image */}
+      <canvas ref={canvasRef} style={{ display: "none" }} />
+      <div className="w-3/4" style={{}}>
+        {/* Video Stream Section */}
         {!capturedImage ? (
-          <div className="flex flex-col items-center mt-8">
-            {/* Fallback: if videoStream hasn't started and no camera is found, show a message */}
-            {!videoStream ? (
-              columns.length > 0 ? (
-                <button
-                  onClick={startCamera}
-                  className="px-5 py-3 font-light tracking-tight capitalize border-2 border-transparent rounded-full text-zinc-200 bg-zinc-700 hover:bg-zinc-900 hover:border-zinc-200"
-                >
-                  Start Camera
-                </button>
-              ) : (
-                <p className="text-zinc-200">No Camera Found</p>
-              )
-            ) : (
-              <button
-                onClick={captureImage}
-                disabled={loading}
-                className={cn(
-                  "relative px-14 py-3 font-light tracking-tight capitalize border-2 border-transparent rounded-full text-zinc-200 bg-indigo-600 transition-all duration-300 overflow-hidden shadow-[0_0_10px_rgba(99,102,241,0.6)]hover:bg-indigo-800 hover:border-indigo-300 hover:shadow-[0_0_20px_rgba(99,102,241,1)] active:scale-95",
-                  loading && "opacity-50 cursor-not-allowed"
-                )}
-              >
-                {/* Sparkles */}
-                <div className="absolute inset-0 overflow-hidden">
-                  {[...Array(6)].map((_, i) => (
-                    <span
-                      key={i}
-                      className="absolute block bg-white rounded-full opacity-50"
-                      style={{
-                        width: `${Math.random() * 4 + 2}px`,
-                        height: `${Math.random() * 4 + 2}px`,
-                        top: `${Math.random() * 100}%`,
-                        left: `${Math.random() * 100}%`,
-                        animation: `sparkle-animation ${
-                          Math.random() * 3 + 2
-                        }s linear infinite`,
-                      }}
-                    />
-                  ))}
-                </div>
-
-                {/* Button Text */}
-                <span className="tracking-wider text-[2vw]">
-                  {loading ? "Capturing..." : "Click here to capture"}
-                </span>
-
-                {/* Magical Styles */}
-                <style>
-                  {`
-      @keyframes sparkle-animation {
-        0% { transform: translate(0, 0) scale(1); opacity: 1; }
-        100% { transform: translate(300%, -50%) scale(0.5); opacity: 0; }
-      }
-    `}
-                </style>
-              </button>
-              // <button
-              //   disabled={loading}
-              //   onClick={captureImage}
-              //   className={cn(
-              //     "capitalize text-zinc-200 tracking-tight font-light bg-zinc-700 py-3 px-5 rounded-full border-2 border-transparent hover:bg-zinc-900 hover:border-zinc-200",
-              //     loading && "opacity-50 cursor-not-allowed"
-              //   )}
-              // >
-              //   {loading ? "Capturing..." : "Click here to capture"}
-              // </button>
+          <div className="flex flex-col items-center py-2">
+            <video
+              ref={videoRef}
+              className="bg-black shadow-lg rounded-2xl w-full"
+              autoPlay
+              muted
+            />
+            {countdown && (
+              <div className="absolute top-0 bottom-0 left-0 right-0 flex items-center justify-center bg-black bg-opacity-50 rounded-2xl">
+                <p className="font-bold text-white animate-ping text-9xl">
+                  {countdown}
+                </p>
+              </div>
             )}
           </div>
         ) : (
-          <div className="flex gap-x-28 mt-8">
+          // Captured Image Section
+          <div className="flex flex-col items-center py-2">
+            <img
+              src={capturedImage}
+              alt="Captured"
+              className="border-4 border-white shadow-lg rounded-2xl w-full"
+              style={{ width: "100%" }}
+            />
+          </div>
+        )}
+      </div>
+
+      {!capturedImage ? (
+        <div className="flex flex-col items-center mt-8">
+          {/* Fallback: if videoStream hasn't started and no camera is found, show a message */}
+          {!videoStream ? (
+            columns.length > 0 ? (
+              <button
+                onClick={startCamera}
+                className="px-5 py-3 font-light tracking-tight capitalize border-2 border-transparent rounded-full text-zinc-200 bg-zinc-700 hover:bg-zinc-900 hover:border-zinc-200"
+              >
+                Start Camera
+              </button>
+            ) : (
+              <p className="text-zinc-200">No Camera Found</p>
+            )
+          ) : (
             <button
-              onClick={() => {
-                startCamera();
-                setCapturedImage(null);
-              }}
-              className="relative px-14 py-3 font-light tracking-tight capitalize border-2 border-transparent rounded-full text-zinc-200 bg-indigo-600 transition-all duration-300 overflow-hidden shadow-[0_0_10px_rgba(99,102,241,0.6)] 
-    hover:bg-indigo-800 hover:border-indigo-300 hover:shadow-[0_0_20px_rgba(99,102,241,1)] active:scale-95"
+              onClick={captureImage}
+              disabled={loading}
+              className={cn(
+                "relative px-14 py-3 font-light tracking-tight capitalize border-2 border-transparent rounded-full text-zinc-200 bg-indigo-600 transition-all duration-300 overflow-hidden shadow-[0_0_10px_rgba(99,102,241,0.6)]hover:bg-indigo-800 hover:border-indigo-300 hover:shadow-[0_0_20px_rgba(99,102,241,1)] active:scale-95",
+                loading && "opacity-50 cursor-not-allowed"
+              )}
             >
               {/* Sparkles */}
               <div className="absolute inset-0 overflow-hidden">
@@ -263,7 +210,9 @@ function Capture() {
               </div>
 
               {/* Button Text */}
-              <span className="tracking-wider text-[2vw]">Retake</span>
+              <span className="tracking-wider text-[2vw]">
+                {loading ? "Capturing..." : "Click here to capture"}
+              </span>
 
               {/* Magical Styles */}
               <style>
@@ -275,7 +224,61 @@ function Capture() {
     `}
               </style>
             </button>
-            {/* <button
+            // <button
+            //   disabled={loading}
+            //   onClick={captureImage}
+            //   className={cn(
+            //     "capitalize text-zinc-200 tracking-tight font-light bg-zinc-700 py-3 px-5 rounded-full border-2 border-transparent hover:bg-zinc-900 hover:border-zinc-200",
+            //     loading && "opacity-50 cursor-not-allowed"
+            //   )}
+            // >
+            //   {loading ? "Capturing..." : "Click here to capture"}
+            // </button>
+          )}
+        </div>
+      ) : (
+        <div className="flex gap-x-28 mt-8">
+          <button
+            onClick={() => {
+              startCamera();
+              setCapturedImage(null);
+            }}
+            className="relative px-14 py-3 font-light tracking-tight capitalize border-2 border-transparent rounded-full text-zinc-200 bg-indigo-600 transition-all duration-300 overflow-hidden shadow-[0_0_10px_rgba(99,102,241,0.6)] 
+    hover:bg-indigo-800 hover:border-indigo-300 hover:shadow-[0_0_20px_rgba(99,102,241,1)] active:scale-95"
+          >
+            {/* Sparkles */}
+            <div className="absolute inset-0 overflow-hidden">
+              {[...Array(6)].map((_, i) => (
+                <span
+                  key={i}
+                  className="absolute block bg-white rounded-full opacity-50"
+                  style={{
+                    width: `${Math.random() * 4 + 2}px`,
+                    height: `${Math.random() * 4 + 2}px`,
+                    top: `${Math.random() * 100}%`,
+                    left: `${Math.random() * 100}%`,
+                    animation: `sparkle-animation ${
+                      Math.random() * 3 + 2
+                    }s linear infinite`,
+                  }}
+                />
+              ))}
+            </div>
+
+            {/* Button Text */}
+            <span className="tracking-wider text-[2vw]">Retake</span>
+
+            {/* Magical Styles */}
+            <style>
+              {`
+      @keyframes sparkle-animation {
+        0% { transform: translate(0, 0) scale(1); opacity: 1; }
+        100% { transform: translate(300%, -50%) scale(0.5); opacity: 0; }
+      }
+    `}
+            </style>
+          </button>
+          {/* <button
             onClick={() => {
               startCamera();
               setCapturedImage(null);
@@ -284,7 +287,7 @@ function Capture() {
           >
             Retake
           </button> */}
-            {/* 
+          {/* 
           <button
             onClick={submitImage}
             className="px-5 py-2 font-light tracking-tight capitalize border-2 border-transparent rounded-full text-zinc-200 bg-zinc-700 hover:bg-zinc-900 hover:border-zinc-200"
@@ -292,46 +295,45 @@ function Capture() {
             Submit
           </button> */}
 
-            <button
-              onClick={submitImage}
-              className="relative px-14 py-3 font-light tracking-tight capitalize border-2 border-transparent rounded-full text-zinc-200 bg-indigo-600 transition-all duration-300 overflow-hidden shadow-[0_0_10px_rgba(99,102,241,0.6)] 
+          <button
+            onClick={submitImage}
+            className="relative px-14 py-3 font-light tracking-tight capitalize border-2 border-transparent rounded-full text-zinc-200 bg-indigo-600 transition-all duration-300 overflow-hidden shadow-[0_0_10px_rgba(99,102,241,0.6)] 
     hover:bg-indigo-800 hover:border-indigo-300 hover:shadow-[0_0_20px_rgba(99,102,241,1)] active:scale-95"
-            >
-              {/* Sparkles */}
-              <div className="absolute inset-0 overflow-hidden">
-                {[...Array(6)].map((_, i) => (
-                  <span
-                    key={i}
-                    className="absolute block bg-white rounded-full opacity-50"
-                    style={{
-                      width: `${Math.random() * 4 + 2}px`,
-                      height: `${Math.random() * 4 + 2}px`,
-                      top: `${Math.random() * 100}%`,
-                      left: `${Math.random() * 100}%`,
-                      animation: `sparkle-animation ${
-                        Math.random() * 3 + 2
-                      }s linear infinite`,
-                    }}
-                  />
-                ))}
-              </div>
+          >
+            {/* Sparkles */}
+            <div className="absolute inset-0 overflow-hidden">
+              {[...Array(6)].map((_, i) => (
+                <span
+                  key={i}
+                  className="absolute block bg-white rounded-full opacity-50"
+                  style={{
+                    width: `${Math.random() * 4 + 2}px`,
+                    height: `${Math.random() * 4 + 2}px`,
+                    top: `${Math.random() * 100}%`,
+                    left: `${Math.random() * 100}%`,
+                    animation: `sparkle-animation ${
+                      Math.random() * 3 + 2
+                    }s linear infinite`,
+                  }}
+                />
+              ))}
+            </div>
 
-              {/* Button Text */}
-              <span className="tracking-wider text-[2vw]">Submit</span>
+            {/* Button Text */}
+            <span className="tracking-wider text-[2vw]">Submit</span>
 
-              {/* Magical Styles */}
-              <style>
-                {`
+            {/* Magical Styles */}
+            <style>
+              {`
       @keyframes sparkle-animation {
         0% { transform: translate(0, 0) scale(1); opacity: 1; }
         100% { transform: translate(300%, -50%) scale(0.5); opacity: 0; }
       }
     `}
-              </style>
-            </button>
-          </div>
-        )}
-      </div>
+            </style>
+          </button>
+        </div>
+      )}
     </div>
   );
 }
