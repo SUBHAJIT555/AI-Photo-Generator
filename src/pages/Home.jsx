b52a703860e8f55b2e6853c754f9c6cd778fd322
SUBHAJIT555 <subhajitdhali0@gmail.com>
2025-03-19 15:18:20 +0400

    home video and bg updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,35 @@
 import { Link } from "react-router-dom";
 import Logo from "../component/Logo";
 import CameraIcon from "../assets/logo/AI_photobooth_icon.png";
+import BackgroundVideo from "../assets/logo/home.mp4"; // Import your video file
 
 function Home() {
   return (
-    <div className="flex flex-col items-center w-full h-screen min-h-screen text-white justify-evenly">
-      <Logo />
-      <div className="text-zinc-100 text-[8vw] flex justify-center items-center">
-        <img className="w-[15vw]" src={CameraIcon} alt="" />
+    <div className="relative flex flex-col items-center w-full h-screen min-h-screen overflow-hidden text-white justify-evenly">
+      {/* Background Video */}
+      <video
+        className="absolute top-0 left-0 w-full h-full object-cover z-[1]"
+        src={BackgroundVideo}
+        autoPlay
+        muted
+        loop
+      />
+
+      {/* Logo */}
+      <div className="z-[2] mb-[15vw]">
+        <Logo />
       </div>
+
+      {/* Icon */}
+      <div className="text-zinc-100 text-[8vw] flex justify-center items-center">
+        <img className="w-[15vw]" src={CameraIcon} alt="Camera Icon" />
+      </div>
+
+      {/* Start Button */}
       <div className="flex items-center justify-center">
         <Link to="/instruction">
           <button
-            className="relative px-14 py-3 font-light tracking-tight capitalize border-2 border-transparent rounded-full text-zinc-200 bg-indigo-600 transition-all duration-300 overflow-hidden shadow-[0_0_10px_rgba(99,102,241,0.6)] 
+            className="relative px-14 py-3 z-[2] font-light tracking-tight capitalize border-2 border-transparent rounded-full text-zinc-200 bg-indigo-600 transition-all duration-300 overflow-hidden shadow-[0_0_10px_rgba(99,102,241,0.6)] 
     hover:bg-indigo-800 hover:border-indigo-300 hover:shadow-[0_0_20px_rgba(99,102,241,1)] active:scale-95"
           >
             {/* Sparkles */}
