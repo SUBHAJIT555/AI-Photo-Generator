aab3f3889da5e18bf3bfadeb819a087fac948624
SUBHAJIT555 <subhajitdhali0@gmail.com>
2026-06-15 11:11:52 +0530

    added video on the home page and remove  the bg and the fuckedup image from the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,27 @@
 import { useNavigate } from "react-router-dom";
 import Logo from "../component/Logo";
-import PageBackground from "../component/PageBackground";
+import homeVideo from "../assets/logo/home.webm";
 import { LiquidMetalButton } from "@/components/ui/LiquidMetalButton";
 
 function Home() {
   const navigate = useNavigate();
   return (
     <div className="flex overflow-hidden relative flex-col items-center w-full h-screen min-h-screen text-white">
-      <PageBackground />
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="absolute inset-0 z-[-1] h-full w-full object-cover"
+      >
+        <source src={homeVideo} type="video/webm" />
+      </video>
 
-      <div className="z-[2] flex flex-1 flex-col items-center justify-center w-full px-4">
+      <div className="z-[2] flex w-full justify-center pt-[8vw]">
         <Logo />
-        <div className="flex flex-1 justify-center items-center w-full -mt-[15vw]">
-          <img
-            src="/home.webp"
-            alt="Home"
-            className="max-h-full w-auto max-w-[70vw] object-contain"
-          />
-        </div>
       </div>
+
+      <div className="flex-1" />
 
       <div className="flex justify-center items-center z-[2] w-full pb-10 mb-[5vw]">
         <LiquidMetalButton
